942ac53c5216097c7ba7d56274dd4c47da039bb8
Muhammad Azhar Rasyad <muhazharrasyad@gmail.com>
2025-10-28 09:58:53 +0700

    Adjust raft variant ports to allow parallel networks

--- raft-variant/network/organizations/ccp-generate.sh
+++ raft-variant/network/organizations/ccp-generate.sh
@@ -27,8 +27,8 @@ function yaml_ccp {
 }
 
 ORG=1
-P0PORT=7051
-CAPORT=7054
+P0PORT=7151
+CAPORT=7154
 PEERPEM=organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
 CAPEM=organizations/peerOrganizations/org1.example.com/ca/ca.org1.example.com-cert.pem
 
@@ -36,8 +36,8 @@ echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrga
 echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/org1.example.com/connection-org1.yaml
 
 ORG=2
-P0PORT=9051
-CAPORT=8054
+P0PORT=9151
+CAPORT=8154
 PEERPEM=organizations/peerOrganizations/org2.example.com/tlsca/tlsca.org2.example.com-cert.pem
 CAPEM=organizations/peerOrganizations/org2.example.com/ca/ca.org2.example.com-cert.pem
 

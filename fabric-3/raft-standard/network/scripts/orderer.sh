e5ea2d9f3dbbe1f8d28b218569cca73f4826087e
Muhammad Azhar Rasyad <muhazharrasyad@gmail.com>
2025-10-29 19:03:11 +0700

    Fix fabric3 raft standard admin endpoint port

--- fabric-3/raft-standard/network/scripts/orderer.sh
+++ fabric-3/raft-standard/network/scripts/orderer.sh
@@ -7,4 +7,4 @@ export PATH=${ROOTDIR}/../bin:${PWD}/../bin:$PATH
 export ORDERER_ADMIN_TLS_SIGN_CERT=${PWD}/organizations/ordererOrganizations/fabric3.standard/orderers/orderer.fabric3.standard/tls/server.crt /dev/null 2>&1
 export ORDERER_ADMIN_TLS_PRIVATE_KEY=${PWD}/organizations/ordererOrganizations/fabric3.standard/orderers/orderer.fabric3.standard/tls/server.key /dev/null 2>&1
 
-osnadmin channel join --channelID ${channel_name} --config-block ./channel-artifacts/${channel_name}.block -o localhost:7053 --ca-file "$ORDERER_CA" --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY" >> log.txt 2>&1
+osnadmin channel join --channelID ${channel_name} --config-block ./channel-artifacts/${channel_name}.block -o localhost:7051 --ca-file "$ORDERER_CA" --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY" >> log.txt 2>&1

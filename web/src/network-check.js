4ab07723fc9ef400d4f7b0d42de3b05ccc0b46ff
muhazharrasyad <muhazharrasyad@gmail.com>
2026-02-02 06:23:39 +0700

    4.2

--- web/src/network-check.js
+++ web/src/network-check.js
@@ -44,8 +44,8 @@ const networkConfigurations = [
         domain: 'fabric3.variant',
         organizationsDir: 'organizations-variant',
         instructions: {
-            up: `cd ${fabric3VariantNetworkDir} && ./network.sh up && ./network.sh createChannel -c fabric3-channel-variant`,
-            deploy: `cd ${fabric3VariantNetworkDir} && ./network.sh deployCC -ccn pelaporan -ccp ../chaincode/pelaporan -ccl node -c fabric3-channel-variant`
+            up: `cd ${smartbftNetworkDir} && ./network.sh up && ./network.sh createChannel -c fabric3-channel-variant`,
+            deploy: `cd ${smartbftNetworkDir} && ./network.sh deployCC -ccn pelaporan -ccp ../chaincode/pelaporan -ccl node -c fabric3-channel-variant`
         }
     }
 ];
